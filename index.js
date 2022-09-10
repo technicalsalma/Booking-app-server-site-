d8feb9b88df4f32e4ea565d0656b03e02be24823
unknown <technicalsalma@gmail.com>
2022-09-10 14:58:12 +0600

    get, post ,put,delete complete

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ mongoose.connection.on("disconnected", ()=>{
     console.log("mangoDB disconnected")
 })
 
+// //middlewares
+// app.use(cors())
+// app.use(cookieParser())
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
