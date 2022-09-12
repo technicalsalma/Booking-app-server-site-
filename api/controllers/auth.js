f50680a74fd0183e6c941e08d638c1e7d57bac9d
unknown <technicalsalma@gmail.com>
2022-09-12 18:36:50 +0600

    jwt token create

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -43,9 +43,9 @@ export const login = async (req, res, next) => {
 
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
-      // .cookie("access_token", token, {
-      //   httpOnly: true,
-      // })
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
       .status(200)
       .json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
